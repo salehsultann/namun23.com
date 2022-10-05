04b79e36f841b15fb38ce84e317696afc8fd3474
salehsultann <110312804+salehsultann@users.noreply.github.com>
2022-10-05 16:04:24 +0300

    namun23.com
    Official repository for namun23.com
    
    Co-Authored-By: KorayKoruyucu <114363969+KorayKoruyucu@users.noreply.github.com>

---
+++ js/links.js
@@ -0,0 +1,63 @@
+function uneplink(){
+    window.open("https://zoom.us/j/2536304247");
+}
+
+function uniceflink(){
+    window.open("https://zoom.us/j/8584914526");
+}
+
+function unicef2link(){
+    window.open("https://zoom.us/j/8646195818");
+}
+
+function uncstdlink(){
+    window.open("https://zoom.us/j/2185457590");
+}
+
+function ioclink(){
+    window.open("https://zoom.us/j/3062296025");
+}
+
+function natolink(){
+    window.open("https://zoom.us/j/9289989982");
+}
+
+function cswlink(){
+    window.open("https://zoom.us/j/2074256064");
+}
+
+function unhcrlink(){
+    window.open("https://zoom.us/j/3565469193");
+}
+
+function openinglink(){
+    window.open("https://zoom.us/j/2074256064");
+}
+
+function coffeelink(){
+    window.open("about:blank");
+}
+
+function drivelink(){
+    window.open("about:blank");
+}
+
+function padletlink(){
+    window.open("https://padlet.com/baharelifbl/uy3kv8cz8wwpyuyk");
+}
+
+function netflixlink1(){
+    window.open("about:blank");
+}
+
+function netflixlink2(){
+    window.open("about:blank");
+}
+
+function discordlink(){
+    window.open("https://discord.gg/nVNVUa2");
+}
+
+function journalslink(){
+    window.open("about:blank");
+}
